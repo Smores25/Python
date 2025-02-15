817b85dbd09bca1210e9ec255c0a230e8f4d61d1
Smores25 <smores25@icloud.com>
2025-02-15 15:24:36 +0000

    test

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 export default defineConfig({
   plugins: [react(), runtimeErrorOverlay(), themePlugin()],
   resolve: {
@@ -18,7 +17,7 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist", "public"), // Ensure output goes to dist/public
+    outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
   },
 });
